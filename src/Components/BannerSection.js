4bb55544312f6eefe835a52000a4cbee6b7c40ca
BolajiOlayinka <bolajiolayinka980@gmail.com>
2020-11-20 17:34:15 +0100

    Banner Area with header

---
+++ src/Components/BannerSection.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/logo.png';
+import FreightShip from '../assets/ship.png';
+import Link from 'react-router-hash-link';
+
+export default function BannerSection() {
+return(
+    <React.Fragment>
+    <Container>
+    <div className="row align-items-center">
+        <div className="col-xl-7 col-lg-7 col-md-6">
+        <LeftSection>
+            <img src={Logo} alt="Digitrad Logo"/>
+            <JapaneseBannerText> 
+            新しい国際販売のご提案
+            </JapaneseBannerText>
+            <EnglishBannerText> 
+            International Digital Trading Service
+            </EnglishBannerText>
+        </LeftSection>
+        
+        </div>
+        <div className="col-xl-5 col-lg-5 col-md-6">
+        <RightSection>
+        <img src={FreightShip} alt="Digitrad Freight Ship"/>
+        </RightSection>
+        </div>
+    </div>
+    <div className="row">
+    <div className="col-xl-3 col-lg-3 col-md-3">
+    <StyledSectionLink to ="/">
+
+    </StyledSectionLink>
+    </div>
+    </div>
+    </Container>
+    </React.Fragment>
+)
+
+}
+
+const Container = styled.div `
+
+@media(min-width:1200px){
+    width:1200px;
+    margin:auto;
+}
+`
+const LeftSection =styled.div `
+
+`
+const RightSection =styled.div `
+img{
+    height:380px;
+}
+`
+const JapaneseBannerText = styled.div `
+width:572px;
+height:46px;
+font-style:normal;
+font-size:36px;
+line-height:49px;
+color:var(--mainBlack);
+margin-top:30.55px;
+`
+const EnglishBannerText = styled.div `
+width:486px;
+height:118px;
+font-style:normal;
+font-weight:300px;
+font-size:48px;
+line-height:56px;
+color:var(--maiinBlack)
+
+`
+
+const StyledSectionLink =styled(Link) `
+
+`
---
+++ src/Components/Header.js
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Icon from "../assets/icon.png";
+
+export default function Header() {
+  const [background, setBackground] = useState("transparent");
+  const [fixed, setFixed] = useState("initial");
+  const handleScroll = () => {
+    if (window.pageYOffset > 0) {
+      setBackground("rgba(0, 0, 0, 0.8)");
+    } else {
+      setBackground("black");
+    }
+  };
+  const fixedScroll = () => {
+    if (window.pageYOffset >= 180) {
+      setFixed("fixed");
+    } else {
+      setFixed("initial");
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+  return (
+    <React.Fragment>
+      <NavWrapper background={background} fixed={fixed}>
+        <Container>
+          <LargeNav>
+            <IconSection>
+              <img src={Icon} alt="Digitrad Icon" />
+            </IconSection>
+            <ItemsContainer>
+              <NavItems>
+                <a href="">ニュース</a>
+              </NavItems>
+              <NavItems>
+                <a href="">お問い合わせ</a>
+              </NavItems>
+              <NavItems>
+                <a href="">ABOUT</a>
+              </NavItems>
+            </ItemsContainer>
+          </LargeNav>
+        </Container>
+      </NavWrapper>
+    </React.Fragment>
+  );
+}
+
+const NavWrapper = styled.div`
+  z-index: 12;
+  display: flex;
+  flex: auto;
+  align-items: center;
+  position: ${(props) => props.fixed} !important;
+
+  background: ${(props) => props.background} !important;
+  transition: 0.4s all;
+  width: -webkit-fill-available;
+  @media (max-width: 576px) {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
+const Container = styled.div`
+  @media (min-width: 1200px) {
+    width: 1200px;
+    margin: auto;
+  }
+`;
+
+const IconSection = styled.div`
+height:45px;
+img{
+    width:45px;
+    height:45px;
+}
+`;
+const LargeNav = styled.div`
+  display: flex;
+  padding-top:25px;
+  padding-bottom:19px;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+const ItemsContainer = styled.div`
+  margin-left: auto;
+  display: flex;
+  align-items:center;
+`;
+const NavItems = styled.div`
+  display: flex;
+
+  height: 30px;
+  margin-right: 40px;
+  :nth-child(1) {
+    width: 113px;
+    a {
+      width: 72px;
+      
+    }
+  }
+  :nth-child(2) {
+    width: 158px;
+    a {
+      width: 108px;
+    }
+  }
+  :nth-child(3) {
+    width: 108px;
+    margin-right: 0px;
+    a {
+      width: 58px;
+    }
+  }
+  a {
+    color: var(--mainGreen);
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24.52px;
+    height: 25px;
+    text-decoration:none;
+    :hover{
+        text-decoration:none;
+    }
+  }
+`;
